81cf41395e0ee5adf59199aa9a83083abc05d1cb
Baboyma Kagniniwa <baboyma@gmail.com>
2024-12-10 15:38:05 -0500

    ALL HANDS - Update visuals

--- Scripts/N26_FY25_TX_NEW_AHD_Results.R
+++ Scripts/N26_FY25_TX_NEW_AHD_Results.R
@@ -91,6 +91,8 @@
 
 # VIZ ====
 
+  ## CrAG
+
   crag <- "Cryptococcal Antigen"
   csf <- "Cerebro-Spinal Fluid"
 
@@ -125,6 +127,7 @@
            AgeCoarse = paste0("Clients: ", AgeCoarse))
 
   viz_ahd_cr <- df_ahd_cr %>%
+    mutate(AgeCoarse = factor(AgeCoarse, c("Clients: All", "Clients: 15+", "Clients: <15"))) %>%
     arrange(AgeCoarse) %>%
     ggplot(aes(y = reorder(Indicator, Target))) +
     geom_col(aes(x = Target), fill = trolley_grey_light) +
@@ -137,12 +140,13 @@
               hjust = -.3, size = 5, color = grey90k) +
     geom_vline(xintercept = 0, color = grey80k) +
     scale_x_continuous(labels = comma, position = "top") +
-    facet_wrap(vars(AgeCoarse), #vars(factor(AgeCoarse, c("Clients: All", "Clients: 15+", "Clients: <15"))),
-                       scales = "free_x", ncol = 1,
+    facet_wrap(vars(AgeCoarse),
+               #vars(factor(AgeCoarse, c("Clients: All", "Clients: 15+", "Clients: <15"))),
+               scales = "free_x", ncol = 1,
                labeller = labeller(AgeCoarse = ~paste0(.x, ", AHD: ", facet_label(df_ahd_cr_sum, .x)))) +
     labs(
       x = "", y = "",
-      caption = glue::glue("Source: USAID/Nigeria - LAMIS+, FY24Q3, Produced on {curr_date()}. Ref. {ref_id}"),
+      caption = glue::glue("Source: USAID/Nigeria - LAMIS+, FY25Q1, Produced on {curr_date()}. Ref. {ref_id}"),
     )+
     si_style_xgrid() +
     theme(
@@ -154,7 +158,162 @@
   viz_ahd_cr
 
   ggsave(plot = viz_ahd_cr,
-         filename = file.path(dir_graphics, "Nigeria - FY24Q3 AHD Monitoring.png"),
+         filename = file.path(dir_graphics, "Nigeria - FY24Q3 AHD CrAg Monitoring.png"),
+         scale = 1.3,
+         dpi = 320,
+         width = 10,
+         height = 6)
+
+  ## CrAG -- All Clients
+
+  viz_ahd_cr2 <- df_ahd_cr %>%
+    filter(AgeCoarse == "Clients: All") %>%
+    ggplot(aes(y = reorder(Indicator, Target))) +
+    geom_col(aes(x = Target), fill = trolley_grey_light) +
+    geom_col(aes(x = Clients), fill = burnt_sienna) +
+    geom_text(aes(x = Clients,
+                  label = paste0(comma(Clients),
+                                 ifelse((!is.na(Prop) & Prop != "0.0%"),
+                                        paste0(" [", Prop, "]"),
+                                        ""))),
+              hjust = -.3, size = 6, color = grey90k) +
+    geom_vline(xintercept = 0, color = grey80k) +
+    scale_x_continuous(labels = comma, position = "top") +
+    facet_wrap(vars(AgeCoarse),
+               #vars(factor(AgeCoarse, c("Clients: All", "Clients: 15+", "Clients: <15"))),
+               scales = "free_x", ncol = 1,
+               labeller = labeller(AgeCoarse = ~paste0(.x, ", AHD: ", facet_label(df_ahd_cr_sum, .x)))) +
+    labs(
+      x = "", y = "",
+      caption = glue::glue("Source: USAID/Nigeria - LAMIS+, FY25Q1, Produced on {curr_date()}. Ref. {ref_id}"),
+    )+
+    si_style_xgrid() +
+    theme(
+      axis.text.x = element_text(size = 12),
+      strip.placement = "outside",
+      strip.text = element_text(size = 14, face = "bold", hjust = 0.04),
+      strip.clip = "off"
+    )
+
+  viz_ahd_cr2
+
+  ggsave(plot = viz_ahd_cr2,
+         filename = file.path(dir_graphics, "Nigeria - FY24Q3 AHD CrAg Monitoring 2.png"),
+         scale = 1.3,
+         dpi = 320,
+         width = 10,
+         height = 6)
+
+
+  ## TB ----
+
+  ind_tb_levels <- c("TX_NEW", "AHD",
+                     "TB Diagnostic Test Done",
+                     "Diagnosed with TB",
+                     "Treated for TB")
+
+  df_ahd_tb_sum <- df_ahd %>%
+    filter(Disease == "Tuberculosis",
+           Indicator %in% c("TX_NEW", "AHD")
+    ) %>%
+    select(-c(Target, Prop)) %>%
+    mutate(AgeCoarse = paste0("Clients: ", AgeCoarse)) %>%
+    summarise(Value = Clients[Indicator == "AHD"],
+              Prop = percent(Clients[Indicator == "AHD"] / Clients[Indicator == "TX_NEW"], .1),
+              .by = c(Disease, AgeCoarse))
+
+  df_ahd_tb <- df_ahd %>%
+    filter(Disease == "Tuberculosis",
+           Indicator %ni% c("TX_NEW", "AHD")
+    ) %>%
+    mutate(Indicator = str_replace(Indicator, "woth", "with"),
+           Indicator = factor(Indicator,
+                              levels = ind_tb_levels,
+                              ordered = T),
+           AgeCoarse = paste0("Clients: ", AgeCoarse))
+
+  viz_ahd_tb <- df_ahd_tb %>%
+    mutate(AgeCoarse = factor(AgeCoarse, c("Clients: All", "Clients: 15+", "Clients: <15"))) %>%
+    arrange(AgeCoarse) %>%
+    ggplot(aes(y = reorder(Indicator, Target))) +
+    geom_col(aes(x = Target), fill = trolley_grey_light) +
+    geom_col(aes(x = Clients), fill = burnt_sienna) +
+    geom_text(aes(x = Clients,
+                  label = paste0(comma(Clients),
+                                 ifelse((!is.na(Prop) & Prop != "0.0%"),
+                                        paste0(" [", Prop, "]"),
+                                        ""))),
+              hjust = -.3, size = 5, color = grey90k) +
+    geom_vline(xintercept = 0, color = grey80k) +
+    scale_x_continuous(labels = comma, position = "top") +
+    facet_wrap(vars(AgeCoarse),
+               #vars(factor(AgeCoarse, c("Clients: All", "Clients: 15+", "Clients: <15"))),
+               scales = "free_x", ncol = 1,
+               labeller = labeller(AgeCoarse = ~paste0(.x, ", AHD: ", facet_label(df_ahd_tb_sum, .x)))) +
+    labs(
+      x = "", y = "",
+      caption = glue::glue("Source: USAID/Nigeria - LAMIS+, FY25Q1, Produced on {curr_date()}. Ref. {ref_id}"),
+    )+
+    si_style_xgrid() +
+    theme(
+      strip.placement = "outside",
+      strip.text = element_text(size = 12, face = "bold", hjust = 0.04),
+      strip.clip = "off"
+    )
+
+  viz_ahd_tb
+
+  ggsave(plot = viz_ahd_tb,
+         filename = file.path(dir_graphics, "Nigeria - FY24Q3 AHD TB Monitoring.png"),
+         scale = 1.3,
+         dpi = 320,
+         width = 10,
+         height = 6)
+
+  df_ahd_tb <- df_ahd %>%
+    filter(Disease == "Tuberculosis",
+           Indicator %ni% c("TX_NEW", "AHD")
+    ) %>%
+    mutate(Indicator = str_replace(Indicator, "woth", "with"),
+           Indicator = factor(Indicator,
+                              levels = ind_tb_levels,
+                              ordered = T),
+           AgeCoarse = paste0("Clients: ", AgeCoarse))
+
+  # TB- All Clients
+  viz_ahd_tb2 <- df_ahd_tb %>%
+    filter(AgeCoarse == "Clients: All") %>%
+    ggplot(aes(y = reorder(Indicator, Target))) +
+    geom_col(aes(x = Target), fill = trolley_grey_light) +
+    geom_col(aes(x = Clients), fill = burnt_sienna) +
+    geom_text(aes(x = Clients,
+                  label = paste0(comma(Clients),
+                                 ifelse((!is.na(Prop) & Prop != "0.0%"),
+                                        paste0(" [", Prop, "]"),
+                                        ""))),
+              hjust = -.3, size = 6, color = grey90k) +
+    geom_vline(xintercept = 0, color = grey80k) +
+    scale_x_continuous(labels = comma, position = "top") +
+    facet_wrap(vars(AgeCoarse),
+               #vars(factor(AgeCoarse, c("Clients: All", "Clients: 15+", "Clients: <15"))),
+               scales = "free_x", ncol = 1,
+               labeller = labeller(AgeCoarse = ~paste0(.x, ", AHD: ", facet_label(df_ahd_tb_sum, .x)))) +
+    labs(
+      x = "", y = "",
+      caption = glue::glue("Source: USAID/Nigeria - LAMIS+, FY25Q1, Produced on {curr_date()}. Ref. {ref_id}"),
+    )+
+    si_style_xgrid() +
+    theme(
+      axis.text.x = element_text(size = 12),
+      strip.placement = "outside",
+      strip.text = element_text(size = 14, face = "bold", hjust = 0.04),
+      strip.clip = "off"
+    )
+
+  viz_ahd_tb2
+
+  ggsave(plot = viz_ahd_tb2,
+         filename = file.path(dir_graphics, "Nigeria - FY24Q3 AHD TB Monitoring 2.png"),
          scale = 1.3,
          dpi = 320,
          width = 10,
